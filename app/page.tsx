25e2ebb8b759adfdc815bb48261b9b4b0d93525d
Adisuarn Wuttikul <adisuarn.w@gmail.com>
2025-01-26 02:41:13 +0700

    Almost Finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,87 @@
-import Image from "next/image";
+import HeroMain from '@/components/hero-main';
+import RightAsideBotany from '@/components/r-aside-botany';
+import { ContactForm } from '@/components/contact-form';
+import { EducationalResources } from '@/components/educational-resources';
+import { cn } from '@/lib/utils';
+import { TreePine, TreeDeciduous } from 'lucide-react';
+import TreeCatagories from '@/public/data/tree-catagories.json';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+interface Catagory {
+  id: number
+  name: string
+  description: string
 }
+
+const Catagories = JSON.parse(JSON.stringify(TreeCatagories))
+
+const Home = () => {
+  return (
+    <main className="w-full h-full">
+
+      <section className="w-full">
+        <HeroMain />
+      </section>
+
+      <section className="w-full flex flex-col items-center">
+        <div className="w-full md:w-[90%] flex p-5 md:grid md:grid-rows-[auto_1fr] gap-4">
+          <aside className="w-7/12 md:row-span-1 md:w-full md:flex md:flex-col md:text-center">
+            <h1 className="text-2xl font-bold">พันธุ์ไม้ที่น่าสนใจ</h1>
+            <p className="mt-2">
+              ตัวอย่างพันธุ์ไม้ที่น่าสนใจ ซึ่งถูกรวบรวมมาจากสถานที่ต่าง ๆ ในโรงเรียนเตรียมอุดมศึกษา
+              โดยแต่ละชนิดมีความโดดเด่นและคุณค่าทางธรรมชาติที่แตกต่างกันไป นอกจากนี้ยังมีพันธุ์ไม้ที่มีประโยชน์ในด้านการศึกษา
+              ซึ่งทางโรงเรียนได้ดูแลรักษาและพัฒนาให้เป็นแหล่งเรียนรู้สำหรับนักเรียน
+            </p>
+          </aside>
+          <RightAsideBotany className="ml-4 md:ml-0 md:row-span-2 md:flex md:w-full" />
+        </div>
+      </section>
+
+      <section className="w-full py-8 bg-[#E3CAA5]/30">
+        <div className="container mx-auto p-5">
+          <h2 className="text-2xl font-bold mb-6 text-[#AD8B73]">หมวดหมู่พันธุ์ไม้</h2>
+          <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 relative">
+              {Catagories.map((catagory: Catagory) => (
+                <div key={catagory.id} className="relative w-full flex flex-col items-center">
+                  <div className="group relative mt-2 border-2 border-[#CEAB93] hover:bg-[#CEAB93] p-2 rounded-full transition-colors duration-200 z-10">
+                    <div className="rounded-full absolute h-[60px] w-[60px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-[#CEAB93]/20 opacity-0 group-hover:opacity-100 transition-all duration-300 -z-5"></div>
+                    {catagory.id % 2 === 0
+                      ? (
+                        <TreePine className="group-hover:text-[#AD8B73] text-[#CEAB93] relative z-10" />
+                      )
+                      : (
+                        <TreeDeciduous className="group-hover:text-[#AD8B73] text-[#CEAB93] relative z-10" />
+                      )
+                    }
+                  </div>
+                  <div className="mb-5 flex flex-col text-center w-full items-center">
+                    <h1 className="mt-2 font-bold text-[#CEAB93]">{catagory.name}</h1>
+                    <p className="w-[80%] text-[#CEAB93]/90">{catagory.description}</p>
+                  </div>
+                  <div className={cn("absolute top-full w-[90%] md:w-[110%] h-[1px] bg-black/20", { 'md:hidden': catagory.id === 5 }, { 'hidden': catagory.id === 6 })}></div>
+                </div>
+              ))}
+              <div className="hidden md:block absolute top-0 left-1/2 h-full w-[1px] bg-black/20 -translate-x-1/2"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full py-8">
+        <div className="container mx-auto p-5">
+          <EducationalResources />
+        </div>
+      </section>
+
+      <section id="contact" className="w-full py-8 bg-[#E3CAA5]/30">
+        <div className="container mx-auto p-5">
+          <h2 className="text-4xl font-bold mb-6 text-center text-[#AD8B73]">ติดต่อเรา</h2>
+          <ContactForm />
+        </div>
+      </section>
+
+    </main>
+  )
+}
+
+export default Home
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: 'placehold.co',
+        port: ''
+      },
+      {
+        protocol: "https",
+        hostname: 'images.app.goo.gl',
+        port: ''
+      },
+      {
+        protocol: "https",
+        hostname: 'www.disthai.com',
+        port: ''
+      },
+      {
+        protocol: "https",
+        hostname: 'upload.wikimedia.org',
+        port: ''
+      },
+      {
+        protocol: "https",
+        hostname: 'kaset.today',
+        port: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
